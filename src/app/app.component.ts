963f46b64f6cba390bfcf252ff40da4d57644952
Alessandro Parente <alessandroparente02@gmail.com>
2025-04-10 16:03:54 +0200

    Added reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { WishItem } from '../shared/models/wishItem';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { WishListComponent } from './wish/wish-list/wish-list.component';
-import {AddWishFormComponent} from './wish/add-wish-form/add-wish-form.component';
 import {CommonModule} from '@angular/common';
-import {WishFilterComponent} from './wish/wish-filter/wish-filter.component';
 import {EventService} from '../shared/services/EventService';
 import {WishService} from './wish/wish.service';
+import {ContactComponent} from './contact/contact.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports:[FormsModule, WishListComponent, AddWishFormComponent, WishFilterComponent, CommonModule],
+    imports:[FormsModule, CommonModule, ContactComponent],
     providers: [EventService, WishService],
     standalone:true
 })
 export class AppComponent {
-  items : WishItem[] = [];
-  filter : any;
 
-  constructor(events: EventService, private wishService: WishService) {
-    events.listen('removeWish', (wish : any) => {
-      // todo remove wish from items
-      let index = this.items.indexOf(wish);
-      this.items.splice(index, 1);
-    })
-  }
+  constructor(){}
 
   ngOnInit(): void {
-    this.wishService.getWishes().subscribe(
-      (data : any) => {
-        this.items = data;
-      },
-      (error : any) => {
-        alert(error.message);
-      }
-    );
+
+
   }
 
 
